1887eebb33ca795ec1700dda19555014b304a77d
ryuyaF <r.fukushima403@gmail.com>
2022-06-09 23:56:21 +0900

    state管理用バケットを社用のものに変更

--- terraform/envs/prod/app/foobar/backend.tf
+++ terraform/envs/prod/app/foobar/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "ryuya-f-laravel-fargate-app-tfstate"
+    bucket = "jcs-ryuya-f-laravel-fargate-app-tfstate"
     key    = "laravel-fargate-app/prod/app/foobar_v1.0.0.tfstate"
     region = "ap-northeast-1"
   }
